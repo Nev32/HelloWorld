87d059a13a151ea544062a15ae4ae6615edd0389
Nevile <nevilers32@gmail.com>
2021-08-15 21:15:00 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+color = input("and what is your favorite color " + name +"? ")
+print(name + " likes " + color)
+
+kg_weight = input("How much do you weight in kilos? ")
+in_pounds = float(kg_weight) * 2.20462262185
+print("Your weight in pounds is "+str(in_pounds))
+
+
