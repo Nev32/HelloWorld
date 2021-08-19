86ca961e2345db61d7a2bf23662e2849a24bcc6a
Nevile <nevilers32@gmail.com>
2021-08-19 00:14:29 -0300

    Day 3
    If Statements
    Logical operators
    Weight converter project

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 name = input("What is your name? ")
 color = input(f"and what is your favorite color {name}? ")
 print(f"{name} likes the color {color}!")
@@ -14,14 +15,48 @@ print(name[0:3])
 #Se o index for negativo, é de trás para frente com o 0 sendo a primeira letra ainda
 print(name[1:-1])
 #deve retornar EVIL
-
+'''
+'''
 #Next is IF STATEMENTS
-
 is_hot = True
+is_cold = True
 
 if is_hot:
     print("Drink lot's of water.")
+elif is_cold:
+    print("Waer warm clothes.")
+else:
+    print("It's a lovely day.")
 print("Have a lovely day")
+'''
+'''
+price = 1000000
+is_good = False
 
+if is_good:
+    down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+    print(f"Down Payment is going to be ${down_payment}.")
+'''
+'''
+name = input("What is your name?")
 
+if len(name) < 3:
+    print("Name must be at least 3 characters length, let's try again.")
+    name = input("What is your name?")
+elif len(name) > 50:
+    print("Name can be a maximum 50 characters length, let's try again.")
+    name = input("What is your name?")
+else:
+    print("Beautiful name!")
+'''
+weight = int(input("Weight:"))
+unit = input("(K)g or (L)bs:")
 
+if unit.upper() == "K":
+    converted_weight = weight * 2.204623
+    print(f"You weight {converted_weight} pounds")
+else:
+    converted_weight = weight * 0.4535924
+    print(f"You weight {converted_weight} kilos")
